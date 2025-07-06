3dffd64dd79ac769bbe30728c8ba1ea93bf8d3ea
Mudaser-Yaseen <mudaser.yaseen@techlogix.com>
2025-07-06 23:07:04 +0500

    workshop testproject initial commit

---
+++ src/test/java/stepdefs/Login.java
@@ -0,0 +1,19 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login {
+    @Given("I am on the SauceDemo login page")
+    public void iAmOnTheSauceDemoLoginPage() {
+        
+    }
+
+    @When("I login with username and password")
+    public void iLoginWithUsernameAndPassword() {
+        
+    }
+
+    @Then("I should be redirected to the inventory page")
+    public void iShouldBeRedirectedToTheInventoryPage() {
+    }
+}
